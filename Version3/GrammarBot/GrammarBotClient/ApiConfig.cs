f01632ff0ec3781ebe6e79d411c6fdc74eeddb1b
chriiischen <bailinch@usc.edu>
2020-04-28 15:20:21 -0700

    Add files via upload

---
+++ Version3/GrammarBot/GrammarBotClient/ApiConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace GrammarBotClient
+{
+    public class ApiConfig
+    {
+        public string Language { get; set; } = "en-US";
+        public string ApiKey { get; set; } = "KS9C5N3Y";
+        public string BaseURI { get; set; } = "http://api.grammarbot.io";
+        public string Version { get; set; } = "v2";
+        public string ApiName { get; set; } = "check";
+    }
+}
---
+++ Version3/GrammarBot/GrammarProgram.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GrammarBotClient;
+using TMPro;
+
+public class GrammarProgram : MonoBehaviour
+{
+
+    private GrammarBot grammarBot;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        grammarBot = new GrammarBotClient.GrammarBot(new GrammarBotClient.ApiConfig());
+        Check();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    async void Check()
+    {
+        var grammar = await grammarBot.CheckAsync("I loves music.");
+        if (grammar.Success)
+        {
+            foreach (var item in grammar.CheckContent.Matches)
+            {
+                Debug.Log(item.offset);
+                Debug.Log(item.Message);
+            }
+        }
+        else 
+        {
+            Debug.Log(grammar.Message);
+        }
+        
+    }
+}
